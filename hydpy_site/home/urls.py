e2c925e3f3a0549764a38d89f7914e3cd043c7f7
Yogesh Kumar <yk.301189@gmail.com>
2016-09-17 17:03:50 +0530

    Updated home model

--- hydpy_site/home/urls.py
+++ hydpy_site/home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import home_page
+from .views import home_page, about_page
 
 
 urlpatterns = [
     url(r'^home/$', home_page, name='homepage'),
+    url(r'^about/$', about_page, name='about_us'),
 ]
--- hydpy_site/home/views.py
+++ hydpy_site/home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home/home.html', {})
+
+
+def about_page(request):
+    return render(request, 'home/about.html', {})
