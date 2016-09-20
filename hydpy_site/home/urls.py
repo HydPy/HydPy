ae594e8a7ebf365e2111c9d54453559932f24954
E Manivannan <elangovan.manivannan@gmail.com>
2016-09-20 23:58:54 +0530

    Added the home and base site page

--- hydpy_site/home/urls.py
+++ hydpy_site/home/urls.py
@@ -3,7 +3,7 @@ from .views import home_page, about_page,events_page
 
 
 urlpatterns = [
-    url(r'^home/$', home_page, name='homepage'),
     url(r'^events/$', events_page, name='events'),
     url(r'^about/$', about_page, name='about_us'),
+    url(r'^', home_page, name='homepage'),
 ]
